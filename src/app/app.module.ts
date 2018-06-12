d0be7c8db77dfd09a99bfb7c22fd505d0c8656c7
Simen A. W. Olsen <so@ayr.no>
2018-06-12 12:45:02 +0200

    fix lint issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BjerkDesignModule } from './bjerk-design/bjerk-design.module';
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
