81b585edde4655067f048d2cb3178755882829bd
DenisKamensky <denis.kamensky@nameone.io>
2018-06-30 23:21:31 +0300

    bl-11 Created layout for content

--- src/app/page-competencies/page-competencies.component.ts
+++ src/app/page-competencies/page-competencies.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-competencies',
   templateUrl: './page-competencies.component.html',
-  styleUrls: ['./page-competencies.component.css']
+  styleUrls: ['./page-competencies.component.scss']
 })
 export class PageCompetenciesComponent implements OnInit {
 
