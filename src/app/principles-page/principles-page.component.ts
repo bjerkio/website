4fa596dc3a5a851983a1c72a5a6bf652ab55510d
DenisKamensky <kengji45@gmail.com>
2018-07-03 14:51:24 +0300

    Created Principles page.

--- src/app/principles-page/principles-page.component.ts
+++ src/app/principles-page/principles-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-principles-page',
   templateUrl: './principles-page.component.html',
-  styleUrls: ['./principles-page.component.css']
+  styleUrls: ['./principles-page.component.scss']
 })
 export class PrinciplesPageComponent implements OnInit {
 
