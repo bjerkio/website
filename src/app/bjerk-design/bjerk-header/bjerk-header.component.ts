9b802535daa47cf35f13c9b87c288a2e5a22a151
DenisKamensky <kengji45@gmail.com>
2018-07-05 14:10:25 +0300

    Added animation to header.

--- src/app/bjerk-design/bjerk-header/bjerk-header.component.ts
+++ src/app/bjerk-design/bjerk-header/bjerk-header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import {  TimelineMax } from 'gsap/TweenMax';
+
+
 
 @Component({
   selector: 'app-bjerk-header',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BjerkHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    this.animate();
+  }
+
+  animate() {
+    if (sessionStorage.headerLoaded && sessionStorage.headerLoaded.length || window.innerWidth < 600) {
+      return;
+    }
+    const el: HTMLElement = this.el.nativeElement;
+    const queue = new TimelineMax()
+      .fromTo(el.querySelector('app-bjerk-logo img'), .5, {opacity: 0, x: -50}, {opacity: 1, x: 0}).staggerFromTo(
+        el.querySelectorAll('app-bjerk-navigation a'), .1, {opacity: 0, y: -50}, {opacity: 1, y: 0}, .1
+      ).call(this.cacheAnimation);
+  }
+
+  /* prevent repeating of header animation */
+  cacheAnimation() {
+    sessionStorage.setItem('headerLoaded', 'loaded');
   }
 
 }
