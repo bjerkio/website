7b5adb97f2280bf4cba8a72f612bc33099abc2ad
DenisKamensky <kengji45@gmail.com>
2018-07-06 16:26:21 +0300

    Added validation.

--- src/app/page-competencies/page-competencies.component.ts
+++ src/app/page-competencies/page-competencies.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-competencies',
   templateUrl: './page-competencies.component.html',
-  styleUrls: ['./page-competencies.component.scss']
 })
 export class PageCompetenciesComponent implements OnInit {
 
