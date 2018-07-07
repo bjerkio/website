77c386a027803c8194b17e4d514789bdf5b6739e
DenisKamensky <kengji45@gmail.com>
2018-07-07 20:53:13 +0300

    created Helper.

--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import gql from 'graphql-tag';
 import { ContentMarketingInterface } from '../interfaces/content-marketing.interface';
 import { ContentMarcetingService } from '../servicies/content-marketing/content-marketing.service';
+import { Utils } from '../helpers/utils';
+import {  TimelineMax } from 'gsap/TweenMax';
+
 
 @Component({
   selector: 'app-page-home',
@@ -12,7 +15,7 @@ import { ContentMarcetingService } from '../servicies/content-marketing/content-
 export class PageHomeComponent implements OnInit {
   cardLinks: ContentMarketingInterface[] = [];
 
-  constructor(apollo: Apollo, private cardLinkService: ContentMarcetingService) {
+  constructor(apollo: Apollo, private cardLinkService: ContentMarcetingService, private el: ElementRef) {
     apollo
       .query({
         query: gql`
@@ -28,10 +31,28 @@ export class PageHomeComponent implements OnInit {
 
   ngOnInit() {
     this.getCardLinks();
+    this.animate();
   }
 
   private getCardLinks(): void {
     this.cardLinkService.getItems().subscribe(cardLinks => this.cardLinks = cardLinks);
+  }
+
+  animate() {
+    const slogan: HTMLElement = this.el.nativeElement.querySelector('.slogan');
+    const {wrappedText, originalText} = Utils.wrappText(slogan);
+    slogan.innerHTML = wrappedText;
+    const queue = new TimelineMax()
+      .staggerFromTo(slogan.querySelectorAll('.wrapped'), .05, {
+        x: -20,
+        y: -40,
+        display: 'inline-block',
+        opacity: 0
+      }, {
+        x: 0,
+        y: 0,
+        opacity: 1
+      }, .05);
   }
 
 }
