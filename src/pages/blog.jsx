7011caad5c51a3d3aef689c39875f8f018de8101
Eullin Chang <eullin@AYR-MAC-004.local>
2019-06-06 10:55:59 +0200

    Responsive fixes

---
+++ src/pages/blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Blog = () => (
+    <div>
+      Blog
+    </div>
+  )
+  
+  Blog.propTypes = {
+    data: PropTypes.object.isRequired,
+  }
+  
+  export default Blog
