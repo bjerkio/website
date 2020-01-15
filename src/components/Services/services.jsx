8b266488ef0ed1640b9528cc5669046c9d2d9435
Eullin Chang <eullinchang@Eullins-iMac.local>
2020-01-15 11:21:06 +0100

    fix: change arrow icon in services

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './services.css';
-import IconArrowRight from '../../images/icon-arrow-right.svg';
 import { Link } from 'gatsby';
 
 const Services = ({ data }) => (
   <Link className="service" to={data.link.uid}>
     <div>{data.title1.text}</div>
-    <img className="service__icon" src={IconArrowRight} alt="Arrow Right" />
+    ➞
   </Link>
 )
 
