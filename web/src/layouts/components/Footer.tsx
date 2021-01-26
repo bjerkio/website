af1e16f9954b3c42f3e632a5fbbc73fc471816c2
Tetiana Dolgopolova <tetiana.dolgopolova@gmail.com>
2021-01-26 19:48:43 +0200

    fix(footer): change footer to a new design

--- web/src/layouts/components/Footer.tsx
+++ web/src/layouts/components/Footer.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Box, Text } from 'theme-ui'
+import { Box, Flex, Link, Text } from 'theme-ui'
 import Container from '../../components/Container'
 import Logo from './Logo'
-import Link from '../../components/Link'
-import ALink from '../../components/ALink'
 
 const Footer: React.FC = props => (
   <Box sx={{ p: 5, mt: 6, pt: 6, bg: 'dark', color: 'white' }} {...props}>
@@ -11,33 +9,44 @@ const Footer: React.FC = props => (
       sx={{
         display: 'grid',
         gridGap: 3, // theme.space[3]
-        gridTemplateColumns: ['1fr', '1fr', '1fr 2fr 1fr'],
+        gridTemplateColumns: ['1fr', '1fr', '1fr', '1fr 0.5fr 1fr 1fr'],
         alignItems: 'center',
       }}
     >
       <Box>
         <Logo textColor="white" width="70px" />
-        <Text sx={{ fontSize: 4, my: 4 }}>
-          Vi vil gjerne jobbe
+        <strong>
+          <Text sx={{ fontSize: 4, my: 4, lineHeight: '22px' }}>
+            Vi vil gjerne jobbe
+            <br />
+            med deg!
+          </Text>
+        </strong>
+        <Text sx={{ fontSize: 2 }}>© 2019 Bjerk. Alle rettigheter.</Text>
+      </Box>
+      <Box sx={{lineHeight: '20px'}}>
+        <Link href="#">+47 22 12 05 12</Link>
+        <br />
+        <Link href="#">kontoret@bjerk.io</Link>
+      </Box>
+      <Box>
+        <Text sx={{ fontSize: 3, lineHeight: '22px' }}>
+          Dronningens gate 8B
           <br />
-          med deg!
+          0151 Oslo
         </Text>
-        <Link to="#">Ta kontakt</Link>
       </Box>
-      <Box sx={{ fontSize: 4 }}>
-        <strong>+47 22 12 05 12</strong>
-        <br />
-        kontoret@bjerk.io
-      </Box>
-      <Box sx={{ fontSize: 3 }}>
-        Dronningens gate 8B, 0151 Oslo
-        <br />
-        <Box sx={{ fontSize: 2, pt: 4 }}>
-          Sjekk oss ut på{' '}
-          <ALink target="_blank" href="http://github.com/bjerkio">
-            Github
-          </ALink>
-        </Box>
+      <Box sx={{ fontSize: 3, textAlign: 'end' }}>
+        <Text sx={{ fontSize: 3, lineHeight: '22px' }}>
+          Vi er alltid på utkikk etter 
+          <br />
+          nye talenter – si hei!
+        </Text>
+        <Text sx={{ fontSize: 2, color: 'primary', mt: 3, lineHeight: '22px' }}>
+          Se ledige stillinger
+          <br />
+          Personvern
+        </Text>
       </Box>
     </Container>
   </Box>
