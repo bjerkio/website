47696e3b340e6737517890aa5205fda4d72d491f
Tetiana Dolgopolova <tetiana.dolgopolova@gmail.com>
2021-02-03 20:14:31 +0200

    feat(about-list): added list component for description

---
+++ web/src/components/about-page/about-list-item.tsx
@@ -0,0 +1,36 @@
+import Img, { FluidObject } from 'gatsby-image';
+import React from 'react';
+import { Box, Container, Grid, Heading, Label } from 'theme-ui';
+
+// TODO: Replace with automatically generated types when available from Sanity.
+// TODO: replace image with gatsby-image when Sanity will be done
+
+export interface AboutListItemProps {
+  title: string;
+  description: string;
+  image?: FluidObject;
+  textAlign: 'left' | 'right';
+}
+
+export const AboutListItem: React.FC<{ data: AboutListItemProps }> = ({
+  data,
+}) => (
+  <Box>
+    <Container sx={{ mt: 6 }}>
+      <Grid gap={2} columns={[1, 1, 2]} px={5}>
+        <Box>
+          <Heading pb={3}>{data.title}</Heading>
+          <Label>{data.description}</Label>
+        </Box>
+        <Box sx={{ gridRow: data.textAlign === 'right' ? 1 : 2 }}>
+          <Img
+            fluid={data.image}
+            durationFadeIn={0}
+            fadeIn={false}
+            draggable={false}
+          />
+        </Box>
+      </Grid>
+    </Container>
+  </Box>
+);
