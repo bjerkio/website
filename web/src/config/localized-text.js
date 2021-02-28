e000d7a7a41b59ae4df452e8feca85460c0aacfb
dkostenko <dkostenko@orion-alliance.com>
2021-02-28 20:33:51 +0300

    feat/add localized text config

---
+++ web/src/config/localized-text.js
@@ -0,0 +1,80 @@
+export default {
+  navbar: {
+    services: {
+      no: 'Tjenester',
+      en: 'Services',
+    },
+    aboutUs: {
+      no: 'Om oss',
+      en: 'About us',
+    },
+    contact: {
+      no: 'Kontakt oss',
+      en: 'Contact us',
+    },
+  },
+  sayHiFooter: {
+    title: {
+      no: 'Si hei!',
+      en: 'Say hi!',
+    },
+    description: {
+      no:
+        'Snakk med oss om ditt prosjekt, så hjelper vi deg fra start til mål.',
+      en:
+        'Talk to us about your project, and we will help you from start to finish.',
+    },
+    fullName: {
+      no: 'Fullt navn',
+      en: 'Full name',
+    },
+    email: {
+      no: 'E-postadresse',
+      en: 'E-mail',
+    },
+    project: {
+      no: 'Prosjektbeskrivelse',
+      en: 'Project description',
+    },
+    budget: {
+      no: 'Budsjett',
+      en: 'Budget',
+    },
+    button: {
+      no: 'Start ditt prosjekt',
+      en: 'Start your project',
+    },
+  },
+  footer: {
+    left: {
+      no: ['Vi vil gjerne jobbe', 'med deg!'],
+      en: ['We want to work', 'with you!'],
+    },
+    contacts: {
+      no: ['+47 22 12 05 12', 'kontoret@bjerk.io'],
+      en: ['+47 22 12 05 12', 'kontoret@bjerk.io'],
+    },
+    address: {
+      no: ['Dronningens gate 8B', '0151 Oslo'],
+      en: ['Dronningens gate 8B', '0151 Oslo'],
+    },
+    right: {
+      no: [
+        'Vi er alltid på utkikk etter',
+        'nye talenter – si hei!',
+        'Se ledige stillinger',
+        'Personvern',
+      ],
+      en: [
+        'We are always looking for',
+        'new talents - say hello!',
+        'See vacancies',
+        'Privacy',
+      ],
+    },
+    copyright: {
+      no: '© 2019 Bjerk. Alle rettigheter.  998 732 867 MVA',
+      en: '© 2019 Bjerk. All rights.  998 732 867 MVA',
+    },
+  },
+};
