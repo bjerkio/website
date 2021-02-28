560a079482c75feebdcc3da2d6f9ba371db4b817
dkostenko <dkostenko@orion-alliance.com>
2021-02-28 11:24:07 +0300

    feat/added new docs in desk-structure

--- studio/desk-structure.js
+++ studio/desk-structure.js
@@ -1,8 +1,20 @@
 import S from '@sanity/desk-tool/structure-builder';
-import { MdHome, MdLayers, MdSettings } from 'react-icons/md';
+import { MdBuild, MdHome, MdLayers, MdSettings } from 'react-icons/md';
 
 const hiddenDocTypes = (listItem) =>
-  !['homepage', 'siteSettings', 'page'].includes(listItem.getId());
+  ![
+    'homepage',
+    'siteSettings',
+    'page',
+    'aboutPage',
+    'contactPage',
+    'footer',
+    'indivPage',
+    'lentPage',
+    'osloJazzPage',
+    'servicesPage',
+    'tabetaltPage',
+  ].includes(listItem.getId());
 
 export default () =>
   S.list()
@@ -26,6 +38,83 @@ export default () =>
             .documentId('homepage'),
         )
         .icon(MdHome),
+      S.listItem()
+        .title('Services Page')
+        .child(
+          S.editor()
+            .id('servicesPage')
+            .schemaType('servicesPage')
+            .documentId('servicesPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('About Page')
+        .child(
+          S.editor()
+            .id('aboutPage')
+            .schemaType('aboutPage')
+            .documentId('aboutPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Contact Page')
+        .child(
+          S.editor()
+            .id('contactPage')
+            .schemaType('contactPage')
+            .documentId('contactPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Indiv Page')
+        .child(
+          S.editor()
+            .id('indivPage')
+            .schemaType('indivPage')
+            .documentId('indivPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Lent Page')
+        .child(
+          S.editor()
+            .id('lentPage')
+            .schemaType('lentPage')
+            .documentId('lentPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Oslo Jazz Page')
+        .child(
+          S.editor()
+            .id('osloJazzPage')
+            .schemaType('osloJazzPage')
+            .documentId('osloJazzPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Tabetalt Page')
+        .child(
+          S.editor()
+            .id('tabetaltPage')
+            .schemaType('tabetaltPage')
+            .documentId('tabetaltPage'),
+        )
+        .icon(MdBuild),
+      S.divider(),
+      S.listItem()
+        .title('Footer')
+        .child(
+          S.editor().id('footer').schemaType('footer').documentId('footer'),
+        )
+        .icon(MdBuild),
+      S.divider(),
       S.listItem()
         .title('Pages')
         .schemaType('page')
