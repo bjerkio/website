9ba6f75a0c5d25adcadd698012484d16b7572dfe
dkostenko <dkostenko@orion-alliance.com>
2021-02-28 16:17:57 +0300

    feat/rename and edit logic in employee`s components

--- web/src/components/contact/employee-list.tsx
+++ web/src/components/contact/employee-list.tsx
@@ -7,7 +7,7 @@ export const EmployeeList: React.FC<{ data: EmployeeListItemProps[] }> = ({
   data = [],
 }) => (
   <List>
-    <Grid gap={1} columns={[1, 2, 3]}>
+    <Grid gap={0} columns={[1, 2, 3]}>
       {data?.map((service, index) => (
         <ListItem key={index}>
           <EmployeeListItem data={service} />
