42f0bfaa934ea0535d31432bc12785a60ca22306
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-17 16:15:41 +0300

    modify call-to-action-box style

--- web/src/components/home-page/call-to-action-box.tsx
+++ web/src/components/home-page/call-to-action-box.tsx
@@ -1,10 +1,11 @@
 import Img from 'gatsby-image';
+import { Link } from 'gatsby-plugin-intl';
 import React, { useState } from 'react';
-import { Box, Heading, Link } from 'theme-ui';
+import { Box, Heading } from 'theme-ui';
 
 export interface CallToActionBoxProps {
   title: string;
-  description: string;
+  description?: string;
   linkTo?: string;
   linkText?: string;
   image?: any;
@@ -23,7 +24,8 @@ const CallToActionBox: React.FC<{ data: CallToActionBoxProps }> = ({
       onMouseOut={() => setHover(false)}
       sx={{
         ':hover': {
-          '.link': {
+          cursor: 'pointer',
+          '.linkText': {
             borderBottom: '2px solid',
           },
         },
@@ -32,28 +34,27 @@ const CallToActionBox: React.FC<{ data: CallToActionBoxProps }> = ({
           textDecoration: 'none',
         },
       }}
+      pr={5}
     >
-      <Box sx={{ width: 100, height: 30, mb: 4 }}>
-        {data.image && data.hoverImage && (
-          <Img
-            fluid={
-              !hover ? data.image.asset.fluid : data.hoverImage.asset.fluid
-            }
-            durationFadeIn={0}
-            fadeIn={false}
-            draggable={false}
-          />
-        )}
-      </Box>
-      <Heading sx={{ mb: 3 }}>{data.title}</Heading>
-      <Box>{children}</Box>
-      <Box mt={4}>
-        {data.linkTo && (
-          <Link className="link" href={data.linkTo}>
-            {data.linkText}
-          </Link>
-        )}
-      </Box>
+      <Link className="link" to={data.linkTo}>
+        <Box sx={{ width: 100, height: 30, mb: 4 }}>
+          {data.image && data.hoverImage && (
+            <Img
+              fluid={
+                !hover ? data.image.asset.fluid : data.hoverImage.asset.fluid
+              }
+              durationFadeIn={0}
+              fadeIn={false}
+              draggable={false}
+            />
+          )}
+        </Box>
+        <Heading sx={{ mb: 3 }}>{data.title}</Heading>
+        <Box>{children}</Box>
+        <Box mt={4}>
+          <span className="linkText">{data.linkText}</span>
+        </Box>
+      </Link>
     </Box>
   );
 };
