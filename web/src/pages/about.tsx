e05bfcfe43e21c3a24c336a2c277114bd96005de
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-17 16:24:19 +0300

    modified props for about-list-item, deleted unnecessary image

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -25,35 +25,32 @@ const AboutPage: React.FC = () => (
         data={[
           {
             title: 'Åpen kildekode',
-            description:
+            descriptionArray: [
               'Vi i Bjerk mener at åpen kildekode er noe av det viktigste i programmering fordi det er en toveis-transaksjon der vi både er forbruker og bidragsyter. En slik tilnærming sikrer også at vi leverer et produkt av høyeste kvalitet. Bjerk har stor tro på at det ikke bare er viktig å samarbeide om å nå målene, men også for å løse problemer innen teknologi. Åpen kildekode gir dessuten kundene våre stor valg- og bevegelsesfrihet om vedkommende velger å samarbeide med noen andre underveis.',
+            ],
             textAlign: 'left',
           },
           {
             title: 'Prosesser',
-            description:
+            descriptionArray: [
               'Trenger du hjelp til litt eller mye? Bjerk påtar seg både små og store oppgaver, så du kan være trygg på at vi hjelper deg gjennom hele prosessen uavhengig av prosjektets størrelse. I begynnelsen av et utviklingsprosjekt kan vi bistå med å planlegge og gjennomføre prosjektering. Deretter vil våre arkitekter sørge for en trygg gjennomføring og utarbeide spesifikasjoner før prosjektet kan tas videre. Bjerk legger stor vekt på å forstå kundenes ønsker. Det er en viktig del av våre prosesser der vi bygger menneskelig forståelse mellom oss og kunden.',
+            ],
             textAlign: 'right',
           },
           {
             title: 'Mennesker',
-            description:
+            descriptionArray: [
               'For å lykkes med en digitaliseringsprosess er det viktig å ivareta menneskene som skal gjennom endringene. I Bjerk har vi lang erfaring med å hjelpe bedrifter gjennom digitaliseringsprosesser. Vi vet at tett dialog og god prosjektledelse er viktige faktorer for at man skal gjøre endringen til en god opplevelse for alle i bedriften. I Bjerk kan vi hjelpe til med en strategi som ivaretar det menneskelige aspektet og de ansatte i bedriften. Vi kan hjelpe deg fra en ide til et ferdig produkt og ta ansvaret for hele eller deler av prosessen.',
+            ],
             textAlign: 'left',
           },
         ]}
       />
-      <Box sx={{ width: '100%', height: 'auto' }}>
+      <Box pt={6} sx={{ width: '100%', height: 'auto' }}>
         <Img fluid={null} durationFadeIn={0} fadeIn={false} draggable={false} />
       </Box>
       <Grid gap={5} columns={[1, 1, 2]} px={5} pt={6}>
         <Box>
-          <Img
-            fluid={null}
-            durationFadeIn={0}
-            fadeIn={false}
-            draggable={false}
-          />
           <Img
             fluid={null}
             durationFadeIn={0}
