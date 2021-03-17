7ebc6637fea1d921ca00189a3c63c50c92f579fa
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-17 16:12:54 +0300

    modify about-list-item style

--- web/src/components/about-page/about-list-item.tsx
+++ web/src/components/about-page/about-list-item.tsx
@@ -7,7 +7,7 @@ import { Box, Container, Grid, Heading, Label } from 'theme-ui';
 
 export interface AboutListItemProps {
   title: string;
-  description: string;
+  descriptionArray: string[];
   image?: FluidObject;
   textAlign: 'left' | 'right';
 }
@@ -18,24 +18,54 @@ export const AboutListItem: React.FC<{ data: AboutListItemProps }> = ({
   <Box>
     <Container sx={{ mt: 6 }}>
       <Grid
-        gap={2}
+        gap={0}
         columns={
-          data.textAlign === 'right' ? [1, 1, '1fr 2fr'] : [1, 1, '2fr 1fr']
+          data.textAlign === 'right' ? [1, 1, '4fr 7fr'] : [1, 1, '7fr 4fr']
         }
         px={5}
       >
-        <Box>
-          <Heading pb={3}>{data.title}</Heading>
-          <Label>{data.description}</Label>
-        </Box>
-        <Box sx={{ gridRow: data.textAlign === 'right' ? 1 : 2 }}>
-          <Img
-            fluid={data.image}
-            durationFadeIn={0}
-            fadeIn={false}
-            draggable={false}
-          />
-        </Box>
+        {data.textAlign === 'right' && (
+          <>
+            <Box px={4}>
+              <Img
+                imgStyle={{ objectFit: 'contain' }}
+                fluid={data.image}
+                durationFadeIn={0}
+                fadeIn={false}
+                draggable={false}
+              />
+            </Box>
+            <Box>
+              <Heading>{data.title}</Heading>
+              {data.descriptionArray.map((descriptionLine, index) => (
+                <Label key={index} mt={3}>
+                  {descriptionLine}
+                </Label>
+              ))}
+            </Box>
+          </>
+        )}
+        {data.textAlign === 'left' && (
+          <>
+            <Box>
+              <Heading>{data.title}</Heading>
+              {data.descriptionArray.map((descriptionLine, index) => (
+                <Label key={index} mt={3}>
+                  {descriptionLine}
+                </Label>
+              ))}
+            </Box>
+            <Box px={4}>
+              <Img
+                imgStyle={{ objectFit: 'contain' }}
+                fluid={data.image}
+                durationFadeIn={0}
+                fadeIn={false}
+                draggable={false}
+              />
+            </Box>
+          </>
+        )}
       </Grid>
     </Container>
   </Box>
