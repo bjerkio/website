1f89c0e0b720f766490fc9cdbf58721cccb5d0bb
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-17 16:14:00 +0300

    modify employee-list-item style

--- web/src/components/contact/employee-list-item.tsx
+++ web/src/components/contact/employee-list-item.tsx
@@ -13,14 +13,14 @@ export interface EmployeeListItemProps {
 export const EmployeeListItem: React.FC<{ data: EmployeeListItemProps }> = ({
   data,
 }) => (
-  <Container mb={[4, 5, 6]}>
+  <Container mb={[3, 4, 5]}>
     <Box
       sx={{
-        width: '12rem',
-        height: '12rem',
-        maxWidth: '343px',
+        width: '25rem',
+        height: '23rem',
+        maxWidth: '370px',
         maxHeight: '346px',
-        my: [3, 4, 5],
+        my: [2, 3, 4],
       }}
     >
       <Img
@@ -32,7 +32,7 @@ export const EmployeeListItem: React.FC<{ data: EmployeeListItemProps }> = ({
     </Box>
     <Text sx={{ fontWeight: 'bold' }}>{data.name}</Text>
     <Text>{data.position}</Text>
-    <Flex mt={4} sx={{ flexDirection: 'column' }}>
+    <Flex mt={2} sx={{ flexDirection: 'column' }}>
       <Link color="black">{data.email}</Link>
       <Link color="black">{data.phoneNumber}</Link>
     </Flex>
