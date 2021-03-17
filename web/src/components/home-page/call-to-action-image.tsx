cc171abad94e85d027cd0d492449f25bc038c78f
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-17 16:18:25 +0300

    added new component to the home page

---
+++ web/src/components/home-page/call-to-action-image.tsx
@@ -0,0 +1,55 @@
+import Img from 'gatsby-image';
+import { Link } from 'gatsby-plugin-intl';
+import React from 'react';
+import { Box } from 'theme-ui';
+
+export interface CallToActionImageProps {
+  linkTo: string;
+  linkText: string;
+  image: any;
+}
+
+const CallToActionImage: React.FC<{ data: CallToActionImageProps }> = ({
+  data,
+}) => {
+  return (
+    <Box
+      sx={{
+        ':hover': {
+          cursor: 'pointer',
+          '.linkText': {
+            borderBottom: '2px solid',
+          },
+          '.ctaImage': {
+            opacity: '.7',
+            transition: '.5s',
+          },
+        },
+        '.link': {
+          color: 'text',
+          textDecoration: 'none',
+        },
+        '.ctaImage': {
+          transition: '.4s',
+        },
+      }}
+    >
+      <Link className="link" to={data.linkTo}>
+        <Box sx={{ mt: 4 }}>
+          <Img
+            className="ctaImage"
+            fluid={data.image.asset.fluid}
+            durationFadeIn={0}
+            fadeIn={false}
+            draggable={false}
+          />
+        </Box>
+        <Box mt={1}>
+          <span className="linkText">{data.linkText}</span>
+        </Box>
+      </Link>
+    </Box>
+  );
+};
+
+export default CallToActionImage;
