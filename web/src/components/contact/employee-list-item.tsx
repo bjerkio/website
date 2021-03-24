b6985dd0cde088f14ab5299e4fcbaa1d53574dff
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-24 14:50:55 +0300

    feat: added Contact page content

--- web/src/components/contact/employee-list-item.tsx
+++ web/src/components/contact/employee-list-item.tsx
@@ -1,9 +1,8 @@
 import { Box, Container, Flex, Link, Text } from '@theme-ui/components';
-import Img, { FluidObject } from 'gatsby-image';
 import React from 'react';
 
 export interface EmployeeListItemProps {
-  photo?: FluidObject;
+  photo?: any;
   name: string;
   position: string;
   email: string;
@@ -11,30 +10,25 @@ export interface EmployeeListItemProps {
 }
 
 export const EmployeeListItem: React.FC<{ data: EmployeeListItemProps }> = ({
-  data,
+  data: { photo, name, position, email, phoneNumber },
 }) => (
-  <Container mb={[4, 5, 6]}>
+  <Container mb={[3, 4, 5]}>
     <Box
       sx={{
-        width: '12rem',
-        height: '12rem',
-        maxWidth: '343px',
+        width: '25rem',
+        height: '23rem',
+        maxWidth: '370px',
         maxHeight: '346px',
-        my: [3, 4, 5],
+        my: [2, 3, 4],
       }}
     >
-      <Img
-        fluid={data.photo}
-        durationFadeIn={0}
-        fadeIn={false}
-        draggable={false}
-      />
+      <img src={`../${photo}`} style={{ width: '100%' }} />
     </Box>
-    <Text sx={{ fontWeight: 'bold' }}>{data.name}</Text>
-    <Text>{data.position}</Text>
-    <Flex mt={4} sx={{ flexDirection: 'column' }}>
-      <Link color="black">{data.email}</Link>
-      <Link color="black">{data.phoneNumber}</Link>
+    <Text sx={{ fontWeight: 'bold' }}>{name}</Text>
+    <Text>{position}</Text>
+    <Flex mt={2} sx={{ flexDirection: 'column' }}>
+      <Link color="black">{email}</Link>
+      <Link color="black">{phoneNumber}</Link>
     </Flex>
   </Container>
 );
--- web/src/pages/contact.tsx
+++ web/src/pages/contact.tsx
@@ -1,10 +1,33 @@
 import { Box, Flex, Heading, Label, Link } from '@theme-ui/components';
 import React from 'react';
 import { SystemStyleObject } from 'theme-ui';
-import MapSvg from '../assets/map.svg';
 import { EmployeeList } from '../components/contact/exployee-list';
 import { Container } from '../components/container';
 import { Layout } from '../components/layouts';
+
+const empoyeeList = [
+  {
+    name: 'Simen A. W. Olsen',
+    position: 'Daglig leder / løsningsarkitekt',
+    email: 'so@bjerk.io',
+    phoneNumber: '+47 953 08 087',
+    photo: 'SimenPhoto.png',
+  },
+  {
+    name: 'Bjørn Niklas Sjøstrøm',
+    position: 'Strategirådgiver',
+    email: 'bns@bjerk.io',
+    phoneNumber: '+47 905 86 616',
+    photo: 'NiklasPhoto.png',
+  },
+  {
+    name: 'Anna Edvardsen',
+    position: 'Prosjektleder',
+    email: 'anna@bjerk.io',
+    phoneNumber: '+47 900 00 000',
+    photo: 'AnnaPhoto.jpeg',
+  },
+];
 
 const styles: SystemStyleObject = {
   '.image': {
@@ -12,9 +35,9 @@ const styles: SystemStyleObject = {
     height: '20rem',
     maxWidth: '396px',
     maxHeight: '328px',
-    right: '15rem',
+    right: '17rem',
     position: 'absolute',
-    top: '15rem',
+    marginTop: '4rem',
     display: ['none', 'none', 'block'],
   },
   '.mobileImage': {
@@ -23,13 +46,12 @@ const styles: SystemStyleObject = {
   },
 };
 
-// TODO: replace image with gatsby-image when sanity will done
 const Contact: React.FC = () => {
   return (
     <Layout>
       <Container>
-        <Box sx={styles}>
-          <Flex>
+        <Box px={0} sx={styles}>
+          <Flex sx={{ mb: 6 }}>
             <Box>
               <Heading
                 as="h1"
@@ -53,36 +75,15 @@ const Contact: React.FC = () => {
                   <Label>0151 Oslo</Label>
                 </Box>
                 <Box className="mobileImage">
-                  <MapSvg />
+                  <img src="../map.svg" style={{ width: '100%' }} />
                 </Box>
               </Flex>
             </Box>
             <Box className="image">
-              <MapSvg />
+              <img src="../map.svg" style={{ width: '100%' }} />
             </Box>
           </Flex>
-          <EmployeeList
-            data={[
-              {
-                name: 'Simen A. W. Olsen',
-                position: 'Daglig leder / løsningsarkitekt',
-                email: 'so@bjerk.io',
-                phoneNumber: '+47 953 08 087',
-              },
-              {
-                name: 'Bjørn Niklas Sjøstrøm',
-                position: 'Strategirådgiver',
-                email: 'bns@bjerk.io',
-                phoneNumber: '+47 905 86 616',
-              },
-              {
-                name: 'Anna Edvardsen',
-                position: 'Prosjektleder',
-                email: 'anna@bjerk.io',
-                phoneNumber: '+47 900 00 000',
-              },
-            ]}
-          />
+          <EmployeeList data={empoyeeList} />
         </Box>
       </Container>
     </Layout>
