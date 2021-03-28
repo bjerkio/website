d92000b22b9e90e01d5842a96987ae0645095c05
Dmitriy Kostenko <dkostenko@klta.net>
2021-03-28 17:15:37 +0300

    feat: contact page mobile view

--- web/src/components/contact/employee-list-item.tsx
+++ web/src/components/contact/employee-list-item.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Link, Text } from '@theme-ui/components';
+import { Box, Container, Flex, Link, Text, Image } from '@theme-ui/components';
 import React from 'react';
 
 export interface EmployeeListItemProps {
@@ -12,23 +12,19 @@ export interface EmployeeListItemProps {
 export const EmployeeListItem: React.FC<{ data: EmployeeListItemProps }> = ({
   data: { photo, name, position, email, phoneNumber },
 }) => (
-  <Container mb={[3, 4, 5]}>
+  <Container mb={5}>
     <Box
       sx={{
-        width: '25rem',
-        height: '23rem',
-        maxWidth: '370px',
-        maxHeight: '346px',
-        my: [2, 3, 4],
+        mr: [ 4, 4, 3 ]
       }}
     >
-      <img src={`../${photo}`} style={{ width: '100%' }} />
+      <Image src={`../${photo}`} sx={{ width: '100%', mb: [ 3, 3, 1   ] }} />
+      <Text sx={{ fontWeight: 'bold' }}>{name}</Text>
+      <Text>{position}</Text>
+      <Flex mt={2} sx={{ flexDirection: 'column' }}>
+        <Link color="black">{email}</Link>
+        <Link color="black">{phoneNumber}</Link>
+      </Flex>
     </Box>
-    <Text sx={{ fontWeight: 'bold' }}>{name}</Text>
-    <Text>{position}</Text>
-    <Flex mt={2} sx={{ flexDirection: 'column' }}>
-      <Link color="black">{email}</Link>
-      <Link color="black">{phoneNumber}</Link>
-    </Flex>
   </Container>
 );
--- web/src/pages/contact.tsx
+++ web/src/pages/contact.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Label, Link } from '@theme-ui/components';
+import { Box, Flex, Heading, Label, Link, Image } from '@theme-ui/components';
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { SystemStyleObject } from 'theme-ui';
@@ -7,20 +7,13 @@ import { Container } from '../components/container';
 import { Layout } from '../components/layouts';
 
 const styles: SystemStyleObject = {
-  mt: 5,
-  '.image': {
-    width: '20rem',
-    height: '20rem',
-    maxWidth: '396px',
-    maxHeight: '328px',
-    right: '17rem',
-    position: 'absolute',
-    marginTop: '4rem',
-    display: ['none', 'none', 'block'],
-  },
+  mt: 6,
+  mb: 6,
+  overflow: 'hidden',
   '.mobileImage': {
-    width: '20rem',
+    width: '50%',
     display: ['block', 'block', 'none'],
+    marginLeft: '50%'
   },
 };
 
@@ -48,44 +41,55 @@ const ContactPage: React.FC = () => {
     }
   `);
 
-  const empoyeeList = edges.map((edge) => edge.node.frontmatter);
+  const employeeList = edges.map((edge) => edge.node.frontmatter);
   return (
     <Layout>
-      <Container>
+      <Container sx={{ pr: [ 0, 0, 6 ] }}>
         <Box sx={styles}>
-          <Flex sx={{ mb: 6 }}>
-            <Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', mb: 6 }}>
+            <Box sx={{ flex: 3 }}>
               <Heading
                 as="h1"
                 sx={{
                   fontWeight: 'normal',
                   width: ['100%', '60%'],
                   my: 5,
+                  pr: [ 4, 4, 0 ]
                 }}
               >
                 Ta kontakt med en av oss for en hyggelig og uforpliktende prat
               </Heading>
-              <Flex>
-                <Box
-                  sx={{
-                    fontSize: [2, 3, 3],
-                  }}
-                >
-                  <Link sx={{ color: 'black' }}>kontoret@bjerk.io</Link>
-                  <Label>+47 22 12 05 12</Label>
-                  <Label>Dronningens gate 8B,</Label>
-                  <Label>0151 Oslo</Label>
-                </Box>
-                <Box className="mobileImage">
-                  <img src="../map.svg" style={{ width: '100%' }} />
-                </Box>
-              </Flex>
+              <Box
+                sx={{
+                  fontSize: [2, 3, 3],
+                }}
+              >
+                <Link sx={{ color: 'black' }}>kontoret@bjerk.io</Link>
+                <Label>+47 22 12 05 12</Label>
+                <Label>Dronningens gate 8B,</Label>
+                <Label>0151 Oslo</Label>
+              </Box>
             </Box>
-            <Box className="image">
-              <img src="../map.svg" style={{ width: '100%' }} />
+            <Box sx={{ flex: 1 }}>
+              <Image 
+                src="../map.svg" 
+                sx={{ 
+                  width: '100%' ,
+                  display: ['none', 'none', 'block'],
+                }}
+              />
             </Box>
-          </Flex>
-          <EmployeeList data={empoyeeList} />
+          </Box>
+          <Image 
+            src="../map.svg" 
+            sx={{
+                width: '50%',
+                display: ['block', 'block', 'none'],
+                marginLeft: '60%',
+                mb: 5
+              }} 
+          />
+          <EmployeeList data={employeeList} />
         </Box>
       </Container>
     </Layout>
