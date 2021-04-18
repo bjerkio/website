8b623fe0310d799fcb7a9462b0397edeab3b7365
Simen A. W. Olsen <so@bjerk.io>
2021-04-18 12:43:36 +0200

    wip

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GlobalStyles } from '../../theme/global-styles';
-import BottomTree from './components/bottom-tree/bottom-tree';
+// import BottomTree from './components/bottom-tree/bottom-tree';
 // import ContactFooter from './components/contact-footer/contact-footer';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
