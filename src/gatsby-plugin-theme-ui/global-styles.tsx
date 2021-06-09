db05ab8c0762322e746a34a8c864ba815313c246
Simen A. W. Olsen <so@bjerk.io>
2021-06-09 16:46:15 +0200

    chore: Bump dependencies (#192)
    Co-authored-by: cobraz <cobraz@users.noreply.github.com>

--- src/gatsby-plugin-theme-ui/global-styles.tsx
+++ src/gatsby-plugin-theme-ui/global-styles.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 
 export const GlobalStyles: React.FC = () => (
   <Global
-    styles={(theme: any) => ({
+    styles={() => ({
       '*': {
         outline: 'none',
       },
       'html, body': {
-        fontFamily: theme.fonts.body,
-        // backgroundColor: theme.colors.dry,
-        // fontSize: '18px',
         textRendering: 'optimizeLegibility',
         '-webkit-font-smoothing': 'antialiased',
         margin: '0px',
