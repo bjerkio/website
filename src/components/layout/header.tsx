5ed3e5ffd25a7dd11072574d02fa05b561c380ce
Brage Sekse Aarset <brage@bjerk.io>
2022-02-02 14:54:13 +0200

    feat: improve layout, add footer content

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,11 @@
 import { Flex } from '@theme-ui/components';
+import { Container } from 'theme-ui';
 import Logo from '../logo';
 
 export const Header: React.FC = () => (
-  <Flex>
-    <Logo sx={{ maxWidth: '100px' }} />
-  </Flex>
+  <Container variant="readable">
+    <Flex sx={{ pt: 4 }}>
+      <Logo sx={{ maxWidth: '100px' }} />
+    </Flex>
+  </Container>
 );
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,28 +1,25 @@
-import { Container, Flex, Grid } from '@theme-ui/components';
+import { Box, Container, Flex, Grid } from '@theme-ui/components';
 import React from 'react';
 import Logo from '../logo';
 import SEO from '../seo';
-import { Footer } from './footer';
+import { Footer } from './footer/footer';
 import { Header } from './header';
 
 export const Layout: React.FC = ({ children }) => (
-  <Container variant="filled">
-    <SEO />
-    <Container variant="readable">
-      <Flex
-        sx={{
-          flexDirection: 'column',
-          minHeight: '100vh',
-          gap: 7,
-          p: 4,
-          pb: 0,
-        }}
-      >
-        <Header />
-        <Flex sx={{ flexDirection: 'column', gap: 7 }}>{children}</Flex>
-
-        <Footer />
-      </Flex>
-    </Container>
-  </Container>
+  <Box sx={{ minHeight: '100vh' }}>
+    <Flex
+      sx={{
+        flexDirection: 'column',
+        gap: 7,
+        pb: 0,
+      }}
+    >
+      <SEO />
+      <Header />{' '}
+      <Container variant="readable">
+        <Flex sx={{ flexDirection: 'column', gap: 7 }}> {children}</Flex>{' '}
+      </Container>
+      <Footer />
+    </Flex>
+  </Box>
 );
