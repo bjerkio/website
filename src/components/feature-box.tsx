b2e9537cdb38a4922dfcd0de00727ff11656e693
Brage Sekse Aarset <brage@bjerk.io>
2022-02-03 14:18:30 +0200

    fix: resolve linter issues

--- src/components/feature-box.tsx
+++ src/components/feature-box.tsx
@@ -1,21 +1,16 @@
-import { Box, Flex } from '@theme-ui/components';
+import { Box, Flex } from 'theme-ui';
 import React from 'react';
 
 export interface FeatureBoxSpec {
   illustration: any;
   textAlign?: 'right' | 'left';
-  ml?: number;
 }
 
 export const FeatureBox: React.FC<FeatureBoxSpec> = ({
   children,
   illustration,
   textAlign = 'left',
-  ml = 4,
 }) => {
-  const leftAlign = ['1fr', '1fr', '4fr 2fr'];
-  const rightAlign = ['1fr', '1fr', '2fr 4fr'];
-
   const firstElement = textAlign === 'left' ? [2, 2, 1] : [2, 1, 2];
   const secondElement = textAlign === 'left' ? [1, 1, 2] : [1, 2, 1];
 
