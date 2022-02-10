d86185a8842c6feb280973ddf52cf166624ff887
Brage Sekse Aarset <brage@bjerk.io>
2022-02-10 13:41:17 +0200

    feat: add navigation

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'next-theme-ui';
 import { Container, Flex } from 'theme-ui';
 import { Logo } from '../logo';
 
-export const Header: React.FC = () => (
-  <Container variant="readable">
-    <Flex sx={{ pt: 2 }}>
-      <Logo sx={{ maxWidth: '100px' }} />
-    </Flex>
-  </Container>
-);
+export const Header: React.FC = () => {
+  return (
+    <Container variant="readable">
+      <Flex sx={{ pt: 2 }}>
+        <Link href={'/'}>
+          <Logo sx={{ maxWidth: '100px', cursor: 'pointer' }} />
+        </Link>
+        <Flex sx={{ gap: 3, mt: 'auto', ml: 'auto', alignItems: 'flex-end' }}>
+          {/** Navigation links go here */}
+        </Flex>
+      </Flex>
+    </Container>
+  );
+};
