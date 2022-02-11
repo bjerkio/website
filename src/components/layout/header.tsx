0a46202f27036e7c728a4a784d1f3e39f34c08f1
Brage Sekse Aarset <brage@bjerk.io>
2022-02-11 12:28:37 +0200

    feat: add nav link in header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'next-theme-ui';
+import { useRouter } from 'next/router';
 import { Container, Flex } from 'theme-ui';
 import { Logo } from '../logo';
 
 export const Header: React.FC = () => {
+  const { pathname } = useRouter();
   return (
     <Container variant="readable">
       <Flex sx={{ pt: 2 }}>
@@ -18,6 +20,17 @@ export const Header: React.FC = () => {
         </Link>
         <Flex sx={{ gap: 3, mt: 'auto', ml: 'auto', alignItems: 'flex-end' }}>
           {/** Navigation links go here */}
+          <Link
+            href={'/about'}
+            sx={{
+              textDecoration: 'none',
+              color: pathname.includes('about') ? 'green80' : 'text',
+              cursor: 'pointer',
+              fontWeight: 'bold',
+            }}
+          >
+            Om oss
+          </Link>
         </Flex>
       </Flex>
     </Container>
