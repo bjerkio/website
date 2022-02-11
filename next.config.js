ced14fd0d8a7ea731bcd474e76cf2b1963148823
Brage Sekse Aarset <brage@bjerk.io>
2022-02-11 17:28:04 +0200

    fix: remove image optimisation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'imgix',
-    path: 'public/',
-  },
 };
 
 module.exports = nextConfig;
