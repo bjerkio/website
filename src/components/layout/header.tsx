ca84aacd2a9f971de76b7f735155db8e9333a545
Brage <brage@bjerk.io>
2022-02-15 15:51:29 +0200

    fix: remove nav links (#348)
    * fix: remove nav links
    
    * fix: remove router
    
    * fix: add text color to theme
    
    * fix: under irrelevant change
    
    * feat: re-add about us link

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -31,17 +31,6 @@ export const Header: React.FC = () => {
           >
             Om oss
           </Link>
-          <Link
-            href={'/principles'}
-            sx={{
-              textDecoration: 'none',
-              color: pathname.includes('principles') ? 'green80' : 'text',
-              cursor: 'pointer',
-              fontWeight: 'bold',
-            }}
-          >
-            Prinsipper
-          </Link>
         </Flex>
       </Flex>
     </Container>
