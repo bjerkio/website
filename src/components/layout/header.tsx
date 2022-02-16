d06ef368910639a0a3c86d3693e0be35ef5e0425
Simen A. W. Olsen <so@bjerk.io>
2022-02-16 08:27:17 +0100

    adjust navigation placement (#357)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'next-theme-ui';
+import { Link, NavLink } from 'next-theme-ui';
 import { useRouter } from 'next/router';
 import { Container, Flex } from 'theme-ui';
 import { Logo } from '../logo';
@@ -7,7 +7,9 @@ export const Header: React.FC = () => {
   const { pathname } = useRouter();
   return (
     <Container variant="readable">
-      <Flex sx={{ pt: 2 }}>
+      <Flex
+        sx={{ pt: 2, justifyContent: 'space-between', alignItems: 'center' }}
+      >
         <Link href={'/'}>
           <Logo
             sx={{
@@ -18,20 +20,17 @@ export const Header: React.FC = () => {
             }}
           />
         </Link>
-        <Flex sx={{ gap: 3, mt: 'auto', ml: 'auto', alignItems: 'flex-end' }}>
-          {/** Navigation links go here */}
-          <Link
-            href={'/about'}
-            sx={{
-              textDecoration: 'none',
-              color: pathname.includes('about') ? 'green80' : 'text',
-              cursor: 'pointer',
-              fontWeight: 'bold',
-            }}
-          >
-            Om oss
-          </Link>
-        </Flex>
+        <NavLink
+          href={'/about'}
+          sx={{
+            textDecoration: 'none',
+            color: pathname.includes('about') ? 'green80' : 'text',
+            cursor: 'pointer',
+            fontWeight: 'bold',
+          }}
+        >
+          Om oss
+        </NavLink>
       </Flex>
     </Container>
   );
