b4b2c89e732aa2dd85e925a547294a61d3ca96db
CalculatedCode <markusmoripen@gmail.com>
2022-06-28 17:04:22 +0200

    fix: capital letter github
    * fix: capital letter github

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -78,7 +78,7 @@ const About: React.FC = () => {
               phone={'+47 970 30 084'}
               email={'markus@bjerk.io'}
               title={'Programvareutvikler'}
-              github={'https://github.com/Moripen'}
+              github={'https://github.com/moripen'}
             />
           </Flex>
           </ContainerWithBackground>
