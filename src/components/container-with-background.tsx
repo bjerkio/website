6f748007b32abb9a0865934cb58dc91f4ad22912
moripen <107696180+moripen@users.noreply.github.com>
2022-06-29 14:04:31 +0200

    feat: compatibility with mobile device and hamburger menu for accessibility (#419)
    * feat: mobile breakpoints
    
    * feat: add breakpoints to text, and to container-with-background to be compatible with mobile.
    * feat: graphical elements now disappear for mobile and the "hamburger" is now visible (not functional atm).
    
    * fix: breakpoints for mobile, graphical element for dropdown
    
    * fix: breakpoints for container-with-background added for less padding when viewed on mobile.
    * feat: new icon for dropdown menu - Still not working.
    
    * fix: githublink
    
    * fix: updated githublink
    
    * feat: hamburgermenu for mobile devices
    
    * feat: hamburgermenu for mobile devices
    
    * fix: linter cleanup
    
    * fix: linter cleanup

--- src/components/container-with-background.tsx
+++ src/components/container-with-background.tsx
@@ -3,34 +3,46 @@ import { Box, Container } from 'theme-ui';
 export interface ContainerWithBackgroundProps {
   color?: 'green' | 'beige';
   element?: 'element1' | 'element2' | 'element3' | 'element4' |'element5' | 'element6';
+  marginbottom?: '-170px';
   positionx?: 'right' | 'left';
-  positiony?: '0' | '10' | '20' | '25' | '30' | '32' | '40' |'50' | '60' |'70' | '80' |'90';
-  paddingtop?: '40px' | '190px' | '210px';
-  paddingbottom?: '210px';
+  positiony?: '0' | '10' | '20' | '25' | '30' | '32' | '40' | '44' | '46' | '50' | '60' |'70' | '80' |'90';
+  paddingtop?: '40px' | '50px' | '70px';
+  paddingbottom?: '90px';
   size?: '453' | '298';
 
 }
 
 export const ContainerWithBackground: React.FC<
   ContainerWithBackgroundProps
-> = ({ children, color, element, paddingbottom, paddingtop, positionx, positiony, size }) => {
+> = ({ children, color, element, marginbottom, paddingbottom, paddingtop, positionx, positiony, size }) => {
   const backgroundColor = color === 'green' ? 'green100' : 'green20';
   const graphicalAddition = `url(/${element}.svg)`;
   return (
+    <Box sx={{
+        visibility: ['visible', 'hidden'],
+        backgroundColor,
+    }}>   
     <Box
       sx={{
         backgroundColor,
-        pt: `${paddingtop}`,
-        pb: `${paddingbottom}`,
+        pt: [0,`${paddingtop}`],
+        pb: [2,`${paddingbottom}`],
+        mb: `${marginbottom}`,
         overflow: 'hidden',
         backgroundImage: graphicalAddition,
         backgroundRepeat: 'no-repeat',
         backgroundSize: [`${size}px`],
         backgroundPosition: `${positionx} bottom`,
-        backgroundPositionY: `${positiony}%`
+        backgroundPositionY: `${positiony}%`,
+        visibility: ['hidden', 'visible'],
       }}
     >
-      <Container variant="readable">{children}</Container>
+     
+      <Container variant="readable" sx={{
+        visibility: ['visible'],
+        p: 7
+      }}>{children}</Container>
+      </Box>
     </Box>
   );
 };
