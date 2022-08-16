54c24302b8117b367c5f1771e14d739694706af9
moripen <markusmoripen@gmail.com>
2022-08-16 20:29:12 +0200

    fix: scalability on images
    * fix: scalability on images
    * fix: shrinks the images with the screen size

--- src/components/about/contact-person.tsx
+++ src/components/about/contact-person.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Flex, Image, Link, Text } from 'theme-ui';
+import { AspectRatio, Box, Flex, Grid, Image, Link, Text } from 'theme-ui';
 
 export interface ContactPersonProps {
   name: string;
@@ -20,64 +20,88 @@ export const ContactPerson: React.FC<ContactPersonProps> = ({
   linkedIn,
 }) => {
   return (
-    <Flex sx={{ flexDirection: 'column', alignSelf: 'center' }}>
-      <Box sx={{ width: '25rem', maxWidth: '25rem' }}>
-        <AspectRatio
-          ratio={459 / 527}
-          sx={{ borderRadius: 6, overflow: 'hidden' }}
-        >
-          <Image
-            src={photoPath}
+    <Flex
+      sx={{
+        flexDirection: 'column',
+        alignSelf: 'center',
+        mb: ['20px', '60px', '60px'],
+      }}
+    >
+      <Grid
+        sx={{
+          display: 'flex',
+          gridTemplateColumns: ['auto', '459px 459px', '459px, 459px'],
+        }}
+      >
+        <Box sx={{ width: '459px' }}>
+          <AspectRatio ratio={459 / 527}>
+            <Image
+              src={photoPath}
+              sx={{
+                height: '100%',
+                width: '100%',
+                maxwidth: '459px',
+                maxheight: '527px',
+                objectFit: 'cover',
+                borderRadius: '6px',
+              }}
+            />
+          </AspectRatio>
+          <Grid
             sx={{
-              minWidth: '100%',
-              minHeight: '100%',
-              maxHeight: '527px',
-              objectFit: 'cover',
+              gridTemplateRows: [
+                'auto',
+                '26px 21px 24px 24px 24px 24px',
+                '26px 21px 24px 24px 24px 24px',
+              ],
             }}
-          />
-        </AspectRatio>
-      </Box>
-      <Text sx={{ fontSize: 2, fontWeight: '600', mt: '20px' }}>{name}</Text>
-      <Text variant="contact">{title}</Text>
-      <Link color="black" href={`tel:${phone}`} mt="20px">
-        {phone}
-      </Link>
-      <Link color="black" href={`mailto:${email}`} mb="20px">
-        {email}
-      </Link>
-      {linkedIn && (
-        <Link
-          variant="socials"
-          href={linkedIn}
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={`LinkedIn Profile for ${name}`}
-          sx={{
-            width: '1.7rem',
-            maxWidth: '35px',
-            cursor: 'pointer',
-          }}
-        >
-          Linkedin
-        </Link>
-      )}
-      {github && (
-        <Link
-          variant="socials"
-          href={github}
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={`Github Profile for ${name}`}
-          sx={{
-            width: '1.7rem',
-            maxWidth: '35px',
-            cursor: 'pointer',
-            mb: '60px',
-          }}
-        >
-          Github
-        </Link>
-      )}
+          >
+            <Text sx={{ fontSize: 2, fontWeight: '600', mt: '20px' }}>
+              {name}
+            </Text>
+            <Text variant="contact">{title}</Text>
+            <Link color="black" href={`tel:${phone}`}>
+              {phone}
+            </Link>
+            <Link color="black" href={`mailto:${email}`} mt="-10px">
+              {email}
+            </Link>
+            {linkedIn && (
+              <Link
+                variant="socials"
+                href={linkedIn}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={`LinkedIn Profile for ${name}`}
+                sx={{
+                  width: '1.7rem',
+                  maxWidth: '35px',
+                  cursor: 'pointer',
+                }}
+              >
+                Linkedin
+              </Link>
+            )}
+            {github && (
+              <Link
+                variant="socials"
+                href={github}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={`Github Profile for ${name}`}
+                sx={{
+                  width: '1.7rem',
+                  maxWidth: '35px',
+                  cursor: 'pointer',
+                  mt: '-10px',
+                }}
+              >
+                Github
+              </Link>
+            )}
+          </Grid>
+        </Box>
+      </Grid>
     </Flex>
   );
 };
