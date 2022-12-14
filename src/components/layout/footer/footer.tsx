f5694cf460df31ecf154f18d73d7ab6d8fc81a86
moripen <markusmoripen@gmail.com>
2022-12-14 10:40:56 +0100

    feat: update import
    * fix: color changed to correct one

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Link, Text } from 'theme-ui';
+import { Box, Container, Flex, Link, Text } from '@chakra-ui/react';
 
 export const Footer: React.FC = () => (
   <Box sx={{ minHeight: '5vh', background: 'dark100', mt: 'auto', p: '30px' }}>
@@ -12,14 +12,14 @@ export const Footer: React.FC = () => (
         }}
       >
         <Flex>
-          <Text>
+          <Text color="green20">
             Kontakt oss på{' '}
             <Link href={'mailto:kontoret@bjerk.io'} variant="footerLink">
               kontoret@bjerk.io
             </Link>
           </Text>
         </Flex>
-        <Flex>© Bjerk 2022</Flex>
+        <Flex sx={{color:"green20"}}>© Bjerk 2022</Flex>
       </Flex>
     </Container>
   </Box>
