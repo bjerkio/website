39895f1fdec37a4d1464e4405538087dad30f9f2
Brage Sekse Aarset <brage@bjerk.io>
2022-12-19 17:29:00 +0100

    feat: set up next 13 and react 18 stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import { GlobalStyles } from '../components/global-styles';
 import { theme } from '../theme';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
-    <GlobalStyles />
-
     <Component {...pageProps} />
   </ThemeProvider>
 );
