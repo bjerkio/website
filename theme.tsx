9ed89945f9fde2c2685007d8b60bedec2798b877
moripen <markusmoripen@gmail.com>
2022-12-20 10:09:55 +0100

    feat: add theme

---
+++ theme.tsx
@@ -0,0 +1,15 @@
+import {
+    extendTheme,
+    type ThemeConfig,
+  } from '@chakra-ui/react';
+  
+  const config: ThemeConfig = {
+    initialColorMode: 'system',
+    useSystemColorMode: true,
+  };
+  
+  const theme = extendTheme({
+    config,
+  });
+
+  export default theme;
