5cb5bd94da947d4bb6754c3bfefc111e055a0060
moripen <markusmoripen@gmail.com>
2022-12-20 11:29:38 +0100

    feat: adjustable fontSizes - Utopia style

--- theme.ts
+++ theme.ts
@@ -10,6 +10,17 @@ import {
   
   const theme = extendTheme({
     config,
+
+    fontSizes: {
+      sm: 'clamp(0.8rem, 0.25vw + 0.74rem, 0.94rem)',
+      base: 'clamp(1rem, 0.45vw + 0.89rem, 1.25rem)',
+      md: 'clamp(1.25rem, 0.76vw + 1.06rem, 1.67rem)',
+      lg: 'clamp(1.56rem, 1.2vw + 1.26rem, 2.22rem)',
+      xl: 'clamp(1.95rem, 1.83vw + 1.5rem, 2.96rem)',
+      xxl: 'clamp(2.44rem, 2.74vw + 1.76rem, 3.95rem)',
+      xxxl: 'clamp(3.05rem, 4.02vw + 2.05rem, 5.26rem)',
+    },
+  
   });
 
   export default theme;
