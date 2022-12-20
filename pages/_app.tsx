40950134fa5157f7964ff060d10281c76fd5ef73
moripen <107696180+moripen@users.noreply.github.com>
2022-12-20 11:27:19 +0100

    feat: add chakra (#480)
    * feat: add chakra-ui dependencies
    
    * feat: add theme
    
    * feat: add ChakraProvider
    
    * feat: add ColorModeScript
    
    * fix: remove default index styling
    
    * feat: flexing Hello, World
    
    * feat: chakra-cli for ref to chakra-theme
    
    * feat: chakra app
    
    * feat: index page
    
    * fix: theme to ts-file
    
    * fix: remove bracket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,21 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import theme from '../theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export interface AppRenderProps {
+  pageProps: object;
+  err?: Error;
+  Component: NextComponentType<NextPageContext, AppRenderProps, object>;
+  router: NextRouter;
+}
+import type { NextComponentType, NextPageContext } from 'next';
+import type { NextRouter } from 'next/router';
+
+export default function App({ Component, pageProps }: AppRenderProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
