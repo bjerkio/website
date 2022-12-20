9126293f70dfa5f1153228b23e67be43b8108b11
moripen <markusmoripen@gmail.com>
2022-12-20 14:39:12 +0100

    fix: correct syntax

--- components/layout/header/hamburger.tsx
+++ components/layout/header/hamburger.tsx
@@ -13,26 +13,26 @@ export const Hamburger: FC<Props> = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M27 21.4336H0V18.2891H27V21.4336Z"
         fill="#0F2040"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M32 12.2891H0V9.14453H32V12.2891Z"
         fill="#0F2040"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M24 3.14459H0V0H24V3.14459Z"
         fill="#0F2040"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M32 3.14459H27V0H32V3.14459Z"
         fill="#0F2040"
       />
