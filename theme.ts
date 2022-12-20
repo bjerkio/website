c6f4bdf34de8402b1b48dc6aea535f9cae3098cd
moripen <markusmoripen@gmail.com>
2022-12-20 11:31:34 +0100

    feat: textStyles

--- theme.ts
+++ theme.ts
@@ -20,7 +20,38 @@ import {
       xxl: 'clamp(2.44rem, 2.74vw + 1.76rem, 3.95rem)',
       xxxl: 'clamp(3.05rem, 4.02vw + 2.05rem, 5.26rem)',
     },
-  
+    textStyles: {
+      caption: {
+        color: 'dark100',
+        fontWeight: 'semibold',
+        fontSize: 'xl',
+      },
+      titlenobold: {
+        color: 'dark100',
+        fontWeight: 'normal',
+        fontSize: 'xl',
+        lineHeight: '3.5rem',
+      },
+      subtitle: {
+        color: 'blue100',
+        fontWeight: 'semibold',
+        fontSize: 'base',
+      },
+      subtitleblack: {
+        color: 'dark100',
+        fontSize: 'base',
+        fontWeight: 'semibold',
+      },
+      standard: {
+        color: 'dark100',
+        fontSize: 'md',
+        fontWeight: 'normal',
+      },
+      contact: {
+        color: 'dark100',
+        fontSize: 'sm',
+      },
+    },
   });
 
   export default theme;
