2401afb3ccc7bb38505a13d24e47825b295bcbe9
moripen <markusmoripen@gmail.com>
2022-12-20 11:22:03 +0100

    fix: remove bracket

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from '@chakra-ui/react';
 
 const Home = () => (
   <Flex>
-        <Text color='green.200' backgroundColor={'pink.100'}>
+        <Text color='green.200' backgroundColor='pink.100'>
             Hello, World!
         </Text>
   </Flex>
