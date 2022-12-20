0d6b700d9ba08db13ab428a99d2a97041294ec2f
moripen <markusmoripen@gmail.com>
2022-12-20 10:12:10 +0100

    feat: add ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import theme from '../theme';
+import { ChakraProvider } from '@chakra-ui/react';
 
+export interface AppRenderProps {
+  pageProps: object;
+  err?: Error;
+  Component: NextComponentType<NextPageContext, AppRenderProps, object>;
+  router: NextRouter;
+}
+import type { NextComponentType, NextPageContext } from 'next';
+import type { NextRouter } from 'next/router';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  <ChakraProvider theme={theme}>
+  <Component {...pageProps} />
+  </ChakraProvider> 
 }
