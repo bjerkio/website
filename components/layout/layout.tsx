6b60a62802d2f210da1012b7ceba90eb294ebe02
moripen <markusmoripen@gmail.com>
2022-12-20 14:27:57 +0100

    feat: layout

---
+++ components/layout/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { SEO } from '../seo';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+
+type Props = {
+  children?: ReactNode;
+};
+
+export const Layout = ({ children }: Props) => (
+  <Flex
+    sx={{
+      flexDirection: 'column',
+      gap: 0,
+      pb: 0,
+    }}
+  >
+    <SEO />
+    <Header />
+    <Flex alignItems="stretch">{children}</Flex>
+
+    <Footer />
+  </Flex>
+);
