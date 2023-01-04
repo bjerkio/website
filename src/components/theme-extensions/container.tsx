9341dee4712ee269092f43d7a7e43f77d2673925
moripen <markusmoripen@gmail.com>
2023-01-04 15:10:26 +0100

    fix: remove unused variant

--- src/components/theme-extensions/container.tsx
+++ src/components/theme-extensions/container.tsx
@@ -2,19 +2,13 @@ import { defineStyleConfig } from '@chakra-ui/react';
 
 export const Container = defineStyleConfig({
   variants: {
-    centered: {
-      margin: '0 auto',
-      m: 0,
-      p: 0,
-      backgroundSize: 'cover',
+    full: {
+      maxWidth: 1500,
     },
     readable: {
       backgroundSize: 'cover',
       maxWidth: 950,
       px: { base: 6, md: 0 },
-    },
-    full: {
-      maxWidth: 1500,
     },
   },
 });
