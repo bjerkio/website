b0bc482f8079fc913acf23434c1dba4832487892
moripen <markusmoripen@gmail.com>
2023-01-04 12:52:27 +0100

    fix: refactor into theme

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -3,30 +3,37 @@ import NextLink from 'next/link'
 import React from 'react';
 
 export const Footer: React.FC = () => (
-  <Box sx={{ minHeight: 7, background: 'dark100', mt: 'auto', p: {base: 5, md: 7}}}>
+  <Box 
+    minHeight= {7}
+    background= 'dark100'
+    mt= 'auto'
+    p= {{base: 5, md: 7}}
+    >
     <Container variant="full">
       <Flex
         sx={{
-          color: 'background',
           justifyContent: 'space-between',
           alignItems: 'center',
-          fontSize: 'base',
           gap: 4
         }}
       >
         <Flex>
-          <Text color="green20">
+          <Text textStyle="footerText">
             Kontakt oss på{' '}
             <Link
               as={NextLink}
               href={'mailto:kontoret@bjerk.io'}
-              variant={{ base: 'footerLink' }}
+              variant='footerLink'
             >
               kontoret@bjerk.io
             </Link>
           </Text>
         </Flex>
-        <Flex sx={{ color: 'green20' }}>© Bjerk 2022</Flex>
+        <Flex>
+          <Text textStyle="footerText">
+            © Bjerk 2022
+          </Text>
+        </Flex>
       </Flex>
     </Container>
   </Box>
