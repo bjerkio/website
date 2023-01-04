85a3ac4babbaecda16b3e2c202bdf1eab3ac592a
moripen <markusmoripen@gmail.com>
2023-01-04 15:30:49 +0100

    fix: alphabetized

--- src/theme.ts
+++ src/theme.ts
@@ -11,11 +11,79 @@ import { Link } from './components/theme-extensions/links';
   };
 
   const theme = extendTheme({
+    breakpoints: {
+      sm: '35rem',
+      md: '66rem',
+      lg: '80rem',
+      xl: '104rem',
+      '2xl': '128rem',
+    },
+    colors: {
+      blue40: '#8FA1FF',
+      blue100: '#2A3BCC',
+      dark100: '#0F2040',
+      foreground: '#ffffff',
+      green20: '#EDF9F1',
+      green100: '#90F494',
+      green120: '#2E896C',
+      muted: '#95E0C8',
+    },
+    components: {
+      Button: {
+        baseStyle: {
+          fontWeight: 'normal',
+          color: 'dark100',
+          borderRadius: 'lg',
+          backgroundColor: 'transparent',
+          fontFamily: 'body',
+        },
+        variants: {
+          primary: {
+          },
+          secondary: {
+            backgroundColor: 'green20',
+            p: 4
+          },
+          with_box: {
+            fontSize: 'base',
+            p: 4,
+            boxShadow:
+            '0.2rem 0.2rem 0.7rem rgba(0, 0, 0, 0.1)',
+          },
+        },
+      },
+      Container,
+      Link,
+    },
     config,
     fonts:{
-      heading: 'Sora',
       body: 'Sora',
+      heading: 'Sora',
       mono: 'Sora'
+    },
+    fontSizes: {
+      sm: 'clamp(0.8rem, 0.25vw + 0.74rem, 0.94rem)',
+      base: 'clamp(1rem, 0.45vw + 0.89rem, 1.25rem)',
+      md: 'clamp(1.25rem, 0.76vw + 1.06rem, 1.67rem)',
+      lg: 'clamp(1.56rem, 1.2vw + 1.26rem, 2.22rem)',
+      xl: 'clamp(1.95rem, 1.83vw + 1.5rem, 2.96rem)',
+      xxl: 'clamp(2.44rem, 2.74vw + 1.76rem, 3.95rem)',
+      xxxl: 'clamp(3.05rem, 4.02vw + 2.05rem, 5.26rem)',
+    },
+    radii: {
+      sm: '0.4rem',
+      md: '1rem',
+      lg: '2rem',
+      xl: '4rem'
+    },
+    // semanticTokens is where we can change darkmode colors.
+    semanticTokens: {
+      colors: {
+        greentodark: {
+          default: 'green100',
+          _dark: 'dark100',
+        },
+      },
     },
     space: {
       0: '0rem',
@@ -29,38 +97,19 @@ import { Link } from './components/theme-extensions/links';
       8: '6rem',
       9: '7rem',
     },
-    colors: {
-      muted: '#95E0C8',
-      green120: '#2E896C',
-      foreground: '#ffffff',
-      green100: '#90F494',
-      blue100: '#2A3BCC',
-      dark100: '#0F2040',
-      green20: '#EDF9F1',
-      blue40: '#8FA1FF',
-    },
-    fontSizes: {
-      sm: 'clamp(0.8rem, 0.25vw + 0.74rem, 0.94rem)',
-      base: 'clamp(1rem, 0.45vw + 0.89rem, 1.25rem)',
-      md: 'clamp(1.25rem, 0.76vw + 1.06rem, 1.67rem)',
-      lg: 'clamp(1.56rem, 1.2vw + 1.26rem, 2.22rem)',
-      xl: 'clamp(1.95rem, 1.83vw + 1.5rem, 2.96rem)',
-      xxl: 'clamp(2.44rem, 2.74vw + 1.76rem, 3.95rem)',
-      xxxl: 'clamp(3.05rem, 4.02vw + 2.05rem, 5.26rem)',
-    },
     textStyles: {
       caption: {
         color: 'dark100',
-        fontWeight: 'semibold',
         fontSize: 'xl',
+        fontWeight: 'semibold',
       },
       contact: {
         color: 'dark100',
         fontSize: 'sm',
       },
       footerText: {
-        color: "green20",
-        fontSize: "base"
+        color: 'green20',
+        fontSize: 'base'
       },
       standard: {
         color: 'dark100',
@@ -69,8 +118,8 @@ import { Link } from './components/theme-extensions/links';
       },
       subtitle: {
         color: 'blue100',
-        fontWeight: 'semibold',
         fontSize: 'base',
+        fontWeight: 'semibold',
       },
       subtitleBlack: {
         color: 'dark100',
@@ -79,57 +128,9 @@ import { Link } from './components/theme-extensions/links';
       },
       titleNoBold: {
         color: 'dark100',
-        fontWeight: 'normal',
         fontSize: 'xl',
+        fontWeight: 'normal',
         lineHeight: '3.5rem',
-      },
-    },
-    breakpoints: {
-      sm: '35rem',
-      md: '66rem',
-      lg: '80rem',
-      xl: '104rem',
-      '2xl': '128rem',
-    },
-    radii: {
-      sm: '0.4rem',
-      md: '1rem',
-      lg: '2rem',
-      xl: '4rem'
-    },
-    semanticTokens: {
-      colors: {
-        greentodark: {
-          default: 'green100',
-          _dark: 'dark100',
-        },
-      },
-    },
-    components: {
-      Container,
-      Link,
-      Button: {
-        baseStyle: {
-          fontWeight: 'normal',
-          color: 'dark100',
-          borderRadius: 'lg',
-          backgroundColor: 'transparent',
-          fontFamily: 'body',
-        },
-        variants: {
-          primary: {
-          },
-          with_box: {
-            fontSize: 'base',
-            p: 4,
-            boxShadow:
-            '0.2rem 0.2rem 0.7rem rgba(0, 0, 0, 0.1)',
-          },
-          secondary: {
-            backgroundColor: 'green20',
-            p: 4
-          },
-        },
       },
     },
   });
