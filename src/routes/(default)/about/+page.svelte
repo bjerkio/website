7601bdba2b5acb85ea0558b35e9279471701184c
Simen A. W. Olsen <so@bjerk.io>
2023-05-28 13:41:22 +0200

    wip

---
+++ src/routes/(default)/about/+page.svelte
@@ -0,0 +1,28 @@
+<div class="container">
+	<div class="section">
+		<p class="highlight">
+			Bjerk er skapt for å forbedre og forenkle måten mennesker lærer, tenker, jobber og
+			kommuniserer på.
+		</p>
+		<p>For oss, er det viktigste vi gjør er å bidra til å gjøre verden til et bedre sted.</p>
+		<a href="/">Les mer om hvordan vi jobber</a>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 60ch;
+	}
+
+	.section,
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: var(--size-md);
+	}
+
+	.highlight {
+		font-size: var(--font-size-lg);
+	}
+</style>
