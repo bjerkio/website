2935dd4a1b68d0e89acac886441df52cffdede4d
Brage Sekse Aarset <brage@bjerk.io>
2023-06-07 13:27:00 +0300

    fix: remove unneeded chatwoot types

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface Window {
-	$chatwoot: {
-		toggle: () => void;
-		toggleBubbleVisibility(option: 'show' | 'hide'): void;
-	};
-	chatwootSettings: {
-		hideMessageBubble: boolean;
-	};
-}
