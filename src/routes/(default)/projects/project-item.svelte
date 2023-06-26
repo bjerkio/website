1ea87ebd39a7d8e0b9e0b812c837e499e51b5f7c
Simen A. W. Olsen <so@bjerk.io>
2023-06-26 22:42:09 +0200

    feat: use white background for project items

--- src/routes/(default)/projects/project-item.svelte
+++ src/routes/(default)/projects/project-item.svelte
@@ -31,7 +31,7 @@
 <style lang="scss">
 	.project {
 		/* border: 5px solid var(--blue20); */
-		background-color: var(--blue20);
+		background-color: #FFFFFF;
 		padding: var(--size-md);
 		border-radius: var(--border-radius-sm);
 
