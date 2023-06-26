0dd5b6a0d71ebaa13043d790d8e835b58f4b39c2
Simen A. W. Olsen <so@bjerk.io>
2023-06-26 15:55:28 +0200

    lint fix

--- src/components/mobile-nav.svelte
+++ src/components/mobile-nav.svelte
@@ -21,6 +21,7 @@
 </button>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <dialog
 	bind:this={dialog}
 	on:close={() => (showModal = false)}
