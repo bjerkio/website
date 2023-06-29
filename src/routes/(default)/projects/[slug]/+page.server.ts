900ed5289b88cee1a442107b9127dca3ba140e32
Simen A. W. Olsen <so@bjerk.io>
2023-06-29 13:54:37 +0200

    fix: slug types

--- src/routes/(default)/projects/[slug]/+page.server.ts
+++ src/routes/(default)/projects/[slug]/+page.server.ts
@@ -1,4 +1,5 @@
 import { error } from '@sveltejs/kit';
+import { invariant } from 'ts-invariant';
 import { getProjects } from '../../../../data/projects/get-projects';
 import { urlFor } from '../../../../data/sanity-client';
 import type { EntryGenerator } from './$types.js';
@@ -9,16 +10,21 @@ export const prerender = true;
 
 export const entries = (() => {
 	const urls = projects
-		.filter((project) => project.slug)
-		.map((project) => ({
-			slug: project.slug.current
-		}));
+		.filter((project) => project.slug?.current)
+		.map((project) => {
+			invariant(project.slug?.current);
+			return {
+				slug: project.slug.current
+			};
+		});
 
 	return urls;
 }) satisfies EntryGenerator;
 
 export const load = async ({ params }) => {
-	const project = projects.find((project) => project.slug.current === params.slug);
+	const project = projects
+		.filter((project) => project.slug?.current)
+		.find((project) => project.slug?.current === params.slug);
 
 	if (!project) {
 		throw error(404, {
