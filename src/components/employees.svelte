b5d2386257c92c94479f4a008758b2ee6c9aaca9
Xillians <sandra.toftemo@bjerk.io>
2023-09-18 16:05:18 +0200

    refactor: convert employee to array

--- src/components/employees.svelte
+++ src/components/employees.svelte
@@ -9,139 +9,176 @@
   import thomas from '$assets/people/thomas.jpg?w=300&h=400';
   import sandra from '$assets/people/sandra.jpg?w=300&h=400';
   import cop from '$assets/people/cop.jpg?w=300&h=400';
+
+  const employees = [
+    // Thomas
+    {
+      givenName: 'Thomas',
+      familyName: 'Brustad',
+      position: 'Rådgiver',
+      phone: '+47 402 86 277',
+      email: 'thomas@bjerk.io',
+      image: thomas,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/thomasbrustad/'
+        },
+        github: {
+          url: 'https://github.com/HanBrustad'
+        }
+      }
+    },
+    // Simen
+    {
+      givenName: 'Simen A. W.',
+      familyName: 'Olsen',
+      position: 'Daglig leder',
+      phone: '+47 95 30 80 87',
+      email: 'simen@bjerk.io',
+      image: simen,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/simenandre/'
+        },
+        github: {
+          url: 'https://github.com/simenandre'
+        }
+      }
+    },
+    // Carl Oscar
+    {
+      givenName: 'Carl Oscar',
+      familyName: 'Petterson',
+      position: 'Rådgiver',
+      phone: '+47 908 59 851',
+      email: 'cop@bjerk.io',
+      image: cop,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/copetterson/'
+        },
+        github: {
+          url: 'https://github.com/copetterson'
+        }
+      }
+    },
+    // Brage
+    {
+      givenName: 'Brage Sekse',
+      familyName: 'Aarset',
+      position: 'Programvareutvikler',
+      phone: '+47 464 65 823',
+      email: 'brage@bjerk.io',
+      image: brage,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/braaar/'
+        },
+        github: {
+          url: 'https://www.linkedin.com/in/braaar/'
+        }
+      }
+    },
+    // Markus
+    {
+      givenName: 'Markus',
+      familyName: 'Moripen',
+      position: 'Programvareutvikler',
+      phone: '+47 970 30 084',
+      email: 'markus@bjerk.io',
+      image: markus,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/moripen/'
+        },
+        github: {
+          url: 'https://github.com/moripen'
+        }
+      }
+    },
+    // Simon
+    {
+      givenName: 'Simon',
+      familyName: 'Rugland',
+      position: 'Programvareutvikler',
+      phone: '+47 474 46 841',
+      email: 'js@bjerk.io',
+      image: simon,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/simon-rugland/'
+        },
+        github: {
+          url: 'https://github.com/simonrugland'
+        }
+      }
+    },
+    // Sandra
+    {
+      givenName: 'Sandra Viktoria',
+      familyName: 'Toftemo',
+      position: 'Programvareutvikler',
+      phone: '+47 406 30 694',
+      email: 'sandra.toftemo@bjerk.io',
+      image: sandra,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/sandra-viktoria-doseth-toftemo-b9b946227/'
+        },
+        github: {
+          url: 'https://github.com/xillians'
+        }
+      }
+    },
+    // Christina
+    {
+      givenName: 'Anna Christina',
+      familyName: 'Lyra',
+      position: 'Markedsføringssjef',
+      phone: '+47 936 86 056',
+      email: 'christina@bjerk.io',
+      image: christina,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/annachristinalyra/'
+        },
+        github: {
+          url: 'https://github.com/ChristinaLyra'
+        }
+      }
+    },
+    // Bjørn Niklas
+    {
+      givenName: 'Bjørn Niklas',
+      familyName: 'Sjøstrøm',
+      position: 'Styreleder',
+      phone: '+47 905 86 616',
+      email: 'bns@bjerk.io',
+      image: bns,
+      socialMedias: {
+        linkedin: {
+          url: 'https://www.linkedin.com/in/bj%C3%B8rn-niklas-sj%C3%B8str%C3%B8m-8043277/'
+        },
+        github: {
+          url: 'https://github.com/seacurrent'
+        }
+      }
+    }
+  ];
 </script>
 
-<Employee
-  givenName="Thomas"
-  familyName="Brustad"
-  position="Rådgiver"
-  phone="+47 402 86 277"
-  email="thomas@bjerk.io"
-  image={thomas}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/thomasbrustad/' },
-    { type: 'github', url: 'https://github.com/HanBrustad' }
-  ]}
-/>
+{#each employees as employee}
+  <Employee
+    givenName={employee.givenName}
+    familyName={employee.familyName}
+    position={employee.position}
+    phone={employee.phone}
+    email={employee.email}
+    image={employee.image}
+    socialMedia={[
+      { type: 'linkedin', url: employee.socialMedias.linkedin.url },
+      { type: 'github', url: employee.socialMedias.github.url }
+    ]}
+  />
+{/each}
 
-<Employee
-  givenName="Simen A. W."
-  familyName="Olsen"
-  position="Daglig leder"
-  phone="+47 95 30 80 87"
-  email="simen@bjerk.io"
-  image={simen}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/simenandre/' },
-    { type: 'github', url: 'https://github.com/simenandre' }
-  ]}
-/>
-
-<Employee
-  givenName="Carl Oscar"
-  familyName="Petterson"
-  position="Rådgiver"
-  phone="+47 908 59 851"
-  email="cop@bjerk.io"
-  image={cop}
-  socialMedia={[
-    {
-      type: 'linkedin',
-      url: 'https://www.linkedin.com/in/copetterson/'
-    },
-    {
-      type: 'github',
-      url: 'https://github.com/copetterson'
-    }
-  ]}
-/>
-
-<Employee
-  givenName="Brage Sekse"
-  familyName="Aarset"
-  position="Programvareutvikler"
-  phone="+47 464 65 823"
-  email="brage@bjerk.io"
-  image={brage}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/braaar/' },
-    { type: 'github', url: 'https://github.com/braaar' }
-  ]}
-/>
-
-<Employee
-  givenName="Markus"
-  familyName="Moripen"
-  position="Programvareutvikler"
-  phone="+47 970 30 084"
-  email="markus@bjerk.io"
-  image={markus}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/moripen/' },
-    { type: 'github', url: 'https://github.com/moripen' }
-  ]}
-/>
-
-<Employee
-  givenName="Simon"
-  familyName="Rugland"
-  position="Programvareutvikler"
-  phone="+47 474 46 841"
-  email="js@bjerk.io"
-  image={simon}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/simon-rugland/' },
-    { type: 'github', url: 'https://github.com/simonrugland' }
-  ]}
-/>
-
-<Employee
-  givenName="Sandra Viktoria"
-  familyName="Toftemo"
-  position="Programvareutvikler"
-  phone="+47 406 30 694"
-  email="sandra.toftemo@bjerk.io"
-  image={sandra}
-  socialMedia={[
-    {
-      type: 'linkedin',
-      url: 'https://www.linkedin.com/in/sandra-viktoria-doseth-toftemo-b9b946227/'
-    },
-    {
-      type: 'github',
-      url: 'https://github.com/xillians'
-    }
-  ]}
-/>
-
-<Employee
-  givenName="Anna Christina"
-  familyName="Lyra"
-  position="Markedsføringssjef"
-  phone="+47 936 86 056"
-  email="christina@bjerk.io"
-  image={christina}
-  socialMedia={[
-    { type: 'linkedin', url: 'https://www.linkedin.com/in/annachristinalyra/' },
-    { type: 'github', url: 'https://github.com/ChristinaLyra' }
-  ]}
-/>
-
-<Employee
-  givenName="Bjørn Niklas"
-  familyName="Sjøstrøm"
-  position="Styreleder"
-  phone="+47 905 86 616"
-  email="bns@bjerk.io"
-  image={bns}
-  socialMedia={[
-    {
-      type: 'linkedin',
-      url: 'https://www.linkedin.com/in/bj%C3%B8rn-niklas-sj%C3%B8str%C3%B8m-8043277/'
-    },
-    {
-      type: 'github',
-      url: 'https://github.com/seacurrent'
-    }
-  ]}
-/>
