4b7f9f89645f8045333b4f0228787a3ba5a9f3e4
Simen A. W. Olsen <so@bjerk.io>
2023-09-25 15:08:59 +0200

    feat: add stories (#612)

---
+++ src/routes/stories/storyheader.svelte
@@ -0,0 +1,39 @@
+<div class="story-header">
+  <h1>Historie ute av kontekst</h1>
+  <p>
+    Bjerk spilte et spill, hvor hver del av historien ble skrevet uten å vite hele konteksten av
+    historien. Kun den forrige del. Dette er hvordan det gikk.
+  </p>
+  <h2>Historier</h2>
+  <ul>
+    <li><a href="/stories/gummibaat">Gummibåt</a></li>
+    <li><a href="/stories/hakkespett">Hakkespett</a></li>
+    <li><a href="/stories/tur-paa-stien">Tur på stien</a></li>
+    <li><a href="/stories/water-pipe">Water Pipe</a></li>
+  </ul>
+</div>
+
+<style>
+  .story-header {
+    max-width: 60ch;
+    margin: 0 auto;
+    padding: var(--size-md);
+  }
+
+  .story-header > * {
+    margin-block-start: 1rem;
+  }
+
+  h2 {
+    padding-bottom: 0px;
+    font-size: 1.1rem;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  }
+</style>
---
+++ src/routes/stories/tur-paa-stien/story.js
@@ -0,0 +1,34 @@
+export const story = [
+  { message: 'Jeg gikk en tur på stien og søkt skogens ro.', author: 'Brage' },
+  { message: 'Da hørte jeg fra lien en gjøk som sa "Ko-ko"!', author: 'Thomas B.' },
+  {
+    message: '*bang* sa det. Gjøken føy avsted. «Hva var det?» skvatt Kine til.',
+    author: 'Simen A. W. Olsen'
+  },
+  {
+    message: '"Slapp av", mumla Svein. "Det var bare discmænen min som dælja i bakken"',
+    author: 'Carl Oscar P.'
+  },
+  { message: '"Du må ta bedre vare på tingene dine!" sa moren strengt.', author: 'Sandra' },
+  { message: '"Men Mamma, det er ikke min feil!", svarte han oppgitt.', author: 'Thomas B.' },
+  {
+    message:
+      '«Nei, nå må du pusse tennene dine» fortsatte moren. Det føltes ut som de hadde stått der i 7 timer og 45 sekunder nå, og ikke kommet noen vei.',
+    author: 'Simen A. W. Olsen'
+  },
+  {
+    message:
+      'Hun tok tannbørsten sakte opp mot munnen. Hendene skalv. Aldri før hadde hun hatt så vondt i magen. Vondt i sjelen. Hun la tannbørsten sakte inn mot fortennene og kjente tannkremen trenge seg gjennom. Spyttet samlet seg raskt opp i munnhulen. "Faen!!" ropte hun, før magen vrengte seg og hele vasken ble fyllt med oppkast.',
+    author: 'Carl Oscar P.'
+  },
+  {
+    message:
+      'Et bank på døren. "Går det bra der inne? Vi må rekke toget om tjue minutter!""Ja, jeg kommer snart!", svarte hun. En løgn, selvsagt. Hun ville helst bare ligge i ro og ikke føle noen ting. Men nå lå hun i fosterstilling på badegulvet og skalv. "Når skal dette ta slutt?", tenkte hun.Lukten av magesyre fylte rommet.',
+    author: 'Brage'
+  },
+  {
+    message:
+      'Alle tanker i hodet hennes forsvant, og det siste hun kunne høre var et flere bank på døren. "Er du sikker på at det går bra?!"',
+    author: 'Sandra'
+  }
+];
