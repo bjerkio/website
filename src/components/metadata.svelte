abeda7fdd2843df2b2aa54dc96ea872c35a801cc
Xillians <sandra.toftemo@bjerk.io>
2023-10-13 14:24:24 +0200

    use metadata object

--- src/components/metadata.svelte
+++ src/components/metadata.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
-  import { MetaTags, type MetaTagsProps } from 'svelte-meta-tags';
-  import type { Seo } from '../data/types';
+  import { MetaTags } from 'svelte-meta-tags';
   import dedent from 'dedent';
   import { urlFor } from '../data/sanity-client-browser';
   import type { Picture } from 'vite-imagetools';
+  import type { Metadata } from '../data/types';
 
-  export let title: string | undefined = undefined;
-  export let description: string | undefined = undefined;
-  export let data: MetaTagsProps = {};
-  export let seo: Seo | null = null;
+  export let data: Metadata = {
+    title: null,
+    searchEngine:{
+      title: null,
+      description: null
+    },
+    socialMedia: {
+      title: null,
+      description: null,
+      images: [{
+        url: null,
+        alt: null
+      }]
+    }
+  };
   export let path: string | undefined = undefined;
 
   export let defaultImage = '/assets/bjerk-default-seo.png';
 
-  export let image: string | Picture = seo?.image
-    ? urlFor(seo.image.asset).size(1200, 630).url()
+  export let image: string = data.socialMedia.images[0]?.url ?
+    urlFor(data.socialMedia.images[0].url).size(1200, 630).url()
     : defaultImage;
 
   function isPicture(image: string | Picture): image is Picture {
@@ -24,8 +35,6 @@
   if (isPicture(image)) {
     image = image.img.src;
   }
-
-  data.title = data.title ?? title;
   const defaultDescription = dedent`
     Bjerk er et dialogdrevet byrå innen digital produktutvikling og programvareutvikling. 
     De siste 12 årene har vi endret måten mennesker lærer, tenker, jobber, og kommuniserer
@@ -35,13 +44,13 @@
 
 <MetaTags
   {...data}
-  titleTemplate={data.title ? '%s – Bjerk' : 'Bjerk - Digitale produktutviklere'}
-  description={seo?.description ?? description ?? data.description ?? defaultDescription}
+  title={data.title ?? 'Bjerk'}
+  description={ data.searchEngine.description ?? defaultDescription }
   canonical={path ? new URL(path, 'https://bjerk.io').toString() : undefined}
   openGraph={{
-    site_name: 'Bjerk',
-    title: seo?.title ?? data.title ?? 'Bjerk',
-    description: seo?.description ?? data.description ?? defaultDescription,
+    siteName: 'Bjerk',
+    title: data.searchEngine.title ?? 'Bjerk',
+    description: data.searchEngine.description ?? defaultDescription,
     images: [
       {
         url: image
@@ -51,8 +60,9 @@
   }}
   twitter={{
     cardType: 'summary_large_image',
-    title: seo?.title ?? data.title ?? 'Bjerk',
-    description: seo?.description ?? data.description ?? defaultDescription,
+    title: data.socialMedia.title ?? data.title ?? 'Bjerk',
+    description: data.socialMedia.description ?? defaultDescription,
     image: image
   }}
+
 />
