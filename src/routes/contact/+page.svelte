b10a640b91fea63db3f91afbab6e1d62370c9d08
Simen A. W. Olsen <so@bjerk.io>
2023-10-14 18:37:22 +0200

    feat: move local business data to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,9 +2,32 @@
   import Metadata from '$components/metadata.svelte';
   import Employees from '$components/employees.svelte';
   import { ArrowSquareOut } from 'phosphor-svelte';
+  import { JsonLd } from 'svelte-meta-tags';
 </script>
 
 <Metadata title="Om oss" path="/contact" />
+
+<JsonLd
+  schema={{
+    '@context': 'https://schema.org',
+    '@type': 'LocalBusiness',
+    name: 'Bjerk AS',
+    url: 'https://bjerk.io/',
+    logo: 'https://bjerk.io/assets/logo.svg',
+    image: 'https://bjerk.io/assets/logo.svg',
+    telephone: '+47 95308087',
+    email: 'office@bjerk.io',
+    taxID: '930 334 693',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Akersgata 51',
+      addressLocality: 'Oslo',
+      addressRegion: 'Oslo',
+      postalCode: '0180',
+      addressCountry: 'NO'
+    }
+  }}
+/>
 
 <div class="container">
   <div class="section elevator">
