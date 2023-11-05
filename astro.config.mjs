d35924ae78d9370af21218a42af1fe5fd67ea7fb
Simen A. W. Olsen <so@bjerk.io>
2023-11-05 16:10:23 +0100

    chore: run formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
 import Icons from 'unplugin-icons/vite';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [Icons({
-      compiler: 'astro'
-    })]
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
   },
   markdown: {
-    remarkPlugins: [remarkGfm]
+    remarkPlugins: [remarkGfm],
   },
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
