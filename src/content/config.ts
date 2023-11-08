a8660f682fa2fdf72707fea7d4118c6563909103
Simen A. W. Olsen <so@bjerk.io>
2023-11-08 20:44:38 +0100

    feat: event page (#655)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,9 @@
-import { defineCollection, reference, z } from 'astro:content';
+import {
+  defineCollection,
+  reference,
+  z,
+  type ImageFunction,
+} from 'astro:content';
 
 const people = defineCollection({
   type: 'data',
@@ -27,6 +32,37 @@ const people = defineCollection({
     }),
 });
 
+const defaultFields = (image: ImageFunction) => ({
+  image: image()
+    .refine(img => img.width >= 300, {
+      message: 'Cover image must be at least 1080 pixels wide!',
+    })
+    .optional(), // Image type validation depends on your implementation
+  title: z.string(),
+  description: z.string(),
+  headline: z.string().optional(),
+  socialMediaTitle: z
+    .string()
+    .describe(
+      `
+            The title for social media platforms.
+            The ideal length is approximately 47 characters.
+            
+            When exceeding this limit, platforms will truncate the title.
+            LinkedIn will truncate the title at 119 characters.
+         `,
+    )
+    .optional(),
+  socialMediaDescription: z.string().optional(),
+  socialMediaImages: z
+    .array(
+      image().refine(img => img.width >= 300, {
+        message: 'Social media images must be at least 1080 pixels wide!',
+      }),
+    )
+    .optional(), // Image type validation depends on your implementation
+});
+
 const services = z.enum([
   'accessiblity',
   'development',
@@ -40,34 +76,7 @@ const project = defineCollection({
   type: 'content',
   schema: ({ image }) =>
     z.object({
-      image: image()
-        .refine(img => img.width >= 300, {
-          message: 'Cover image must be at least 1080 pixels wide!',
-        })
-        .optional(), // Image type validation depends on your implementation
-      title: z.string(),
-      description: z.string(),
-      headline: z.string().optional(),
-      socialMediaTitle: z
-        .string()
-        .describe(
-          `
-            The title for social media platforms.
-            The ideal length is approximately 47 characters.
-            
-            When exceeding this limit, platforms will truncate the title.
-            LinkedIn will truncate the title at 119 characters.
-         `,
-        )
-        .optional(),
-      socialMediaDescription: z.string().optional(),
-      socialMediaImages: z
-        .array(
-          image().refine(img => img.width >= 300, {
-            message: 'Social media images must be at least 1080 pixels wide!',
-          }),
-        )
-        .optional(), // Image type validation depends on your implementation
+      ...defaultFields(image),
       customer: z.string().min(1), // Assuming reference to organization is a string identifier
       dateFrom: z.date(),
       dateTo: z.date().optional(),
@@ -89,34 +98,7 @@ const post = defineCollection({
   type: 'content',
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      description: z.string(),
-      headline: z.string().optional(),
-      image: image()
-        .refine(img => img.width >= 300, {
-          message: 'Cover image must be at least 1080 pixels wide!',
-        })
-        .optional(),
-      socialMediaTitle: z
-        .string()
-        .describe(
-          `
-            The title for social media platforms.
-            The ideal length is approximately 47 characters.
-            
-            When exceeding this limit, platforms will truncate the title.
-            LinkedIn will truncate the title at 119 characters.
-         `,
-        )
-        .optional(),
-      socialMediaDescription: z.string().optional(),
-      socialMediaImages: z
-        .array(
-          image().refine(img => img.width >= 300, {
-            message: 'Social media images must be at least 1080 pixels wide!',
-          }),
-        )
-        .optional(),
+      ...defaultFields(image),
       date: z.date(),
       author: reference('people')
         .or(z.array(reference('people')))
@@ -125,8 +107,109 @@ const post = defineCollection({
     }),
 });
 
+/**
+ * @url https://schema.org/Offer
+ */
+const eventOffer = z.object({
+  url: z.string(),
+  price: z.string(),
+  availability: z.enum([
+    'https://schema.org/InStock',
+    'https://schema.org/SoldOut',
+    'https://schema.org/PreOrder',
+    'https://schema.org/AvailableForPurchase',
+    'https://schema.org/OutOfStock',
+    'https://schema.org/Discontinued',
+  ]),
+  validFrom: z.string().optional(),
+  validThrough: z.string().optional(),
+});
+
+/**
+ * @url https://schema.org/Organization
+ */
+const eventOrganization = (image: ImageFunction) =>
+  z.object({
+    type: z.literal('Organization'),
+    name: z.string(),
+    url: z.string(),
+    logo: image().optional(),
+  });
+
+/**
+ * @url https://schema.org/Person
+ */
+const eventPerson = (image: ImageFunction) =>
+  z.object({
+    type: z.literal('Person'),
+    name: z.string(),
+    url: z.string(),
+    jobTitle: z.string().optional(),
+    image: image().optional(),
+  });
+
+/**
+ * @url https://schema.org/performer
+ */
+const eventPerformer = (image: ImageFunction) =>
+  z.union([eventOrganization(image), eventPerson(image)]);
+
+const event = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      ...defaultFields(image),
+      startDate: z.date(),
+      endDate: z.date(),
+      eventAttendanceMode: z.enum([
+        'https://schema.org/OnlineEventAttendanceMode',
+        'https://schema.org/OfflineEventAttendanceMode',
+        'https://schema.org/MixedEventAttendanceMode',
+      ]),
+      eventStatus: z.enum([
+        'https://schema.org/EventScheduled',
+        'https://schema.org/EventCancelled',
+        'https://schema.org/EventMovedOnline',
+        'https://schema.org/EventPostponed',
+        'https://schema.org/EventRescheduled',
+        'https://schema.org/EventLive',
+      ]),
+      location: z
+        .object({
+          name: z.string(),
+          address: z.object({
+            streetAddress: z.string(),
+            addressLocality: z.string(),
+            postalCode: z.string(),
+            addressCountry: z.string(),
+          }),
+        })
+        .default({
+          name: 'Bjerk',
+          address: {
+            streetAddress: 'Akersgata 51',
+            addressLocality: 'Oslo',
+            postalCode: '0180',
+            addressCountry: 'NO',
+          },
+        }),
+      offers: z.array(eventOffer).nonempty(),
+      performer: z
+        .array(eventPerformer(image))
+        .nonempty()
+        .default([
+          {
+            type: 'Organization',
+            name: 'Bjerk',
+            url: 'https://bjerk.io',
+          },
+        ]),
+    }),
+});
+
 export const collections = {
   project,
   people,
   post,
+  event,
 };
