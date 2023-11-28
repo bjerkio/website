c1e4a1d174c3ddbdc57e94cf165cc3515976bcb6
Simen A. W. Olsen <so@bjerk.io>
2023-11-28 14:05:28 +0100

    feat: use biome for lint and format
    Prettier can be removed once support for HTML, CSS and Markdown is ready. This is a smooth transition.
    
    Read more: https://biomejs.dev/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import remarkGfm from 'remark-gfm';
-import Icons from 'unplugin-icons/vite';
+import { defineConfig } from "astro/config";
+import remarkGfm from "remark-gfm";
+import Icons from "unplugin-icons/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bjerk.io',
+  site: "https://bjerk.io",
   vite: {
     plugins: [
       Icons({
-        compiler: 'astro',
+        compiler: "astro",
       }),
     ],
   },
